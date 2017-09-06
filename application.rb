b7068316193720ed460cf0e25cc08c9539c85f17
Katherine Lynch <katherly@upenn.edu>
2017-09-06 16:56:53 -0400

    WIP recreate marc21 and structural for DLA-compliant requests

--- application.rb
+++ application.rb
@@ -16,7 +16,72 @@ use Rack::Logger
 class Record < ActiveRecord::Base
 end
 
-def create_record(bib_id, blob, format)
+def create_record(bib_id, format, options = {})
+  case format
+    when 'marc21'
+      bibs_url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs'
+      alma_key = ENV['ALMA_KEY']
+      return logger.warn('No key available') if alma_key.nil?
+      source_blob = ''
+      path = "#{bibs_url}/?mms_id=#{bib_id}&expand=p_avail&apikey=#{alma_key}"
+
+      begin
+        open(path) { |io| source_blob = io.read }
+      rescue => exception
+        return logger.warn("#{exception.message} returned by source for #{bib_id}")
+      end
+
+      return logger.warn("No record data available at source for #{bib_id}") if source_blob.empty?
+
+      reader = Nokogiri::XML(source_blob)
+      record = reader.xpath('//bibs/bib/record')
+
+      holdings = {}
+
+      for i in 0..(record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children.length-1)
+        holdings[i] = { :holding_id => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children[i].text,
+                        :call_number => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="d"]').children[i].text,
+                        :library => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="b"]').children[i].text,
+                        :location => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="j"]').children[i].text
+        }
+      end
+
+      Nokogiri::XML::Builder.with(reader.at('record')) do |xml|
+        xml.holdings {
+          holdings.each do |holding_key, holding|
+            xml.holding {
+              xml.holding_id holding[:holding_id]
+              xml.call_number holding[:call_number]
+              xml.library holding[:library]
+              xml.location holding[:location]
+            }
+          end
+        }
+      end
+
+      record.search('//record/datafield[@tag="INT"]').remove
+      record.search('//record/datafield[@tag="INST"]').remove
+      record.search('//record/datafield[@tag="AVA"]').remove
+
+      record = reader.xpath('//bibs/bib/record')
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim', 'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
+          xml << record.to_xml
+        }
+      end
+      blob = builder.to_xml
+    when 'structural'
+      return logger.warn("No sceti_prefix specified for #{bib_id}") if options[:sceti_prefix].nil?
+      blob = with_structural_metadata(bib_id, options[:sceti_prefix])
+    when 'dla'
+      return logger.warn("No sceti_prefix specified for #{bib_id}") if options[:sceti_prefix].nil?
+      create_record(bib_id, 'structural', options)
+      create_record(bib_id, 'marc21')
+
+      binding.pry
+    else
+      return logger.warn("Invalid format specified: #{format}")
+  end
   record = Record.find_or_initialize_by(:bib_id => bib_id, :format => format)
   record.format = format
   record.blob = blob
@@ -24,8 +89,8 @@ def create_record(bib_id, blob, format)
   record.save!
 end
 
-def with_structural_metadata(bib_id, legacy_prefix)
-  structural_endpoint = "http://dla.library.upenn.edu/dla/#{legacy_prefix.downcase}/pageturn.xml?id=#{legacy_prefix.upcase}_#{legacy_bib_id(bib_id)}"
+def with_structural_metadata(bib_id, sceti_prefix)
+  structural_endpoint = "http://dla.library.upenn.edu/dla/#{sceti_prefix.downcase}/pageturn.xml?id=#{sceti_prefix.upcase}_#{legacy_bib_id(bib_id)}"
   data = Nokogiri::XML.parse(open(structural_endpoint))
   pages = data.xpath('//xml/page')
   record = Nokogiri::XML::Builder.new do |xml|
@@ -36,7 +101,7 @@ def with_structural_metadata(bib_id, legacy_prefix)
       }
     }
   end
-  return record
+  return record.to_xml
 end
 
 def legacy_bib_id(bib_id)
@@ -83,80 +148,23 @@ class Application < Sinatra::Base
     end
   end
 
-  LEGACY_PREFIXES = %w[MEDREN PRINT]
+  SCETI_PREFIXES = %w[MEDREN PRINT]
   AVAILABLE_FORMATS = %w[marc21 structural dla]
 
-  bibs_url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs'
-  alma_key = ENV['ALMA_KEY']
-
   get '/records/:bib_id/create/?' do |bib_id|
-    return 'No key available' if alma_key.nil?
-
     format = params[:format].nil? ? 'marc21' : params[:format]
 
     record_check = Record.find_or_initialize_by(:bib_id => bib_id, :format => format)
-
     fresh = record_check.updated_at.nil? ? false : fresh_check(bib_id, record_check.updated_at)
-
     redirect "/records/#{bib_id}/show?format=#{format}" if fresh
 
     case format
       when 'structural'
-        return 'Specify legacy_prefix' if params[:legacy_prefix].nil?
-        structural_record = with_structural_metadata(bib_id, params[:legacy_prefix])
-        create_record(bib_id, structural_record.to_xml, 'structural')
+         create_record(bib_id, 'structural', params)
       when 'marc21'
-        blob = ''
-        path = "#{bibs_url}/?mms_id=#{bib_id}&expand=p_avail&apikey=#{alma_key}"
-
-        begin
-          open(path) { |io| blob = io.read }
-        rescue => exception
-          return "#{exception.message} returned by source for #{bib_id}"
-        end
-
-        return "No record data available at source for #{bib_id}" if blob.empty?
-
-        reader = Nokogiri::XML(blob)
-        record = reader.xpath('//bibs/bib/record')
-
-        holdings = {}
-
-        for i in 0..(record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children.length-1)
-          holdings[i] = { :holding_id => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children[i].text,
-                          :call_number => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="d"]').children[i].text,
-                          :library => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="b"]').children[i].text,
-                          :location => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="j"]').children[i].text
-          }
-        end
-
-        Nokogiri::XML::Builder.with(reader.at('record')) do |xml|
-          xml.holdings {
-            holdings.each do |holding_key, holding|
-              xml.holding {
-                xml.holding_id holding[:holding_id]
-                xml.call_number holding[:call_number]
-                xml.library holding[:library]
-                xml.location holding[:location]
-              }
-            end
-          }
-        end
-
-        record.search('//record/datafield[@tag="INT"]').remove
-        record.search('//record/datafield[@tag="INST"]').remove
-        record.search('//record/datafield[@tag="AVA"]').remove
-
-        record = reader.xpath('//bibs/bib/record')
-        builder = Nokogiri::XML::Builder.new do |xml|
-          xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim', 'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
-            xml << record.to_xml
-          }
-        end
-
-        create_record(bib_id, builder.to_xml, 'marc21')
+        create_record(bib_id, 'marc21')
       when 'dla'
-        binding.pry
+        create_record(bib_id, 'dla', params)
       else
         return "Invalid specified format #{format}"
     end
@@ -189,10 +197,10 @@ class Application < Sinatra::Base
     end
   end
 
-  %w[/legacy_prefixes/? /records/legacy_prefixes/?].each do |path|
+  %w[/sceti_prefixes/? /records/sceti_prefixes/?].each do |path|
     get path do
-      @legacy_prefixes = LEGACY_PREFIXES
-      erb :legacy_prefixes
+      @sceti_prefixes = SCETI_PREFIXES
+      erb :sceti_prefixes
     end
   end
 
