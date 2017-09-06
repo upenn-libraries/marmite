4951f87306d821de8322ea98f133bcbfc1e207a9
Katherine Lynch <katherly@upenn.edu>
2017-09-06 16:18:47 -0400

    WIP framework for adding DLA format

--- application.rb
+++ application.rb
@@ -84,7 +84,7 @@ class Application < Sinatra::Base
   end
 
   LEGACY_PREFIXES = %w[MEDREN PRINT]
-  AVAILABLE_FORMATS = %w[marc21 structural]
+  AVAILABLE_FORMATS = %w[marc21 structural dla]
 
   bibs_url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs'
   alma_key = ENV['ALMA_KEY']
@@ -92,7 +92,7 @@ class Application < Sinatra::Base
   get '/records/:bib_id/create/?' do |bib_id|
     return 'No key available' if alma_key.nil?
 
-    format = params[:structural_metadata].nil? ? 'marc21' : 'structural'
+    format = params[:format].nil? ? 'marc21' : params[:format]
 
     record_check = Record.find_or_initialize_by(:bib_id => bib_id, :format => format)
 
@@ -100,123 +100,68 @@ class Application < Sinatra::Base
 
     redirect "/records/#{bib_id}/show?format=#{format}" if fresh
 
-    if params[:structural_metadata]
-      return 'Specify legacy_prefix' if params[:legacy_prefix].nil?
-      structural_record = with_structural_metadata(bib_id, params[:legacy_prefix])
-      create_record(bib_id, structural_record.to_xml, 'structural')
-      redirect "/records/#{bib_id}/show?format=structural"
-    end
+    case format
+      when 'structural'
+        return 'Specify legacy_prefix' if params[:legacy_prefix].nil?
+        structural_record = with_structural_metadata(bib_id, params[:legacy_prefix])
+        create_record(bib_id, structural_record.to_xml, 'structural')
+      when 'marc21'
+        blob = ''
+        path = "#{bibs_url}/?mms_id=#{bib_id}&expand=p_avail&apikey=#{alma_key}"
 
-    blob = ''
-    path = "#{bibs_url}/?mms_id=#{bib_id}&expand=p_avail&apikey=#{alma_key}"
+        begin
+          open(path) { |io| blob = io.read }
+        rescue => exception
+          return "#{exception.message} returned by source for #{bib_id}"
+        end
 
-    begin
-      open(path) { |io| blob = io.read }
-    rescue => exception
-      return "#{exception.message} returned by source for #{bib_id}"
-    end
+        return "No record data available at source for #{bib_id}" if blob.empty?
 
-    return "No record data available at source for #{bib_id}" if blob.empty?
+        reader = Nokogiri::XML(blob)
+        record = reader.xpath('//bibs/bib/record')
 
-    reader = Nokogiri::XML(blob)
-    record = reader.xpath('//bibs/bib/record')
+        holdings = {}
 
-    holdings = {}
-
-    for i in 0..(record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children.length-1)
-      holdings[i] = { :holding_id => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children[i].text,
-                      :call_number => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="d"]').children[i].text,
-                      :library => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="b"]').children[i].text,
-                      :location => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="j"]').children[i].text
-      }
-    end
-
-    if params[:structural_metadata]
-      return 'Specify legacy_prefix' if params[:legacy_prefix].nil?
-      structural_record = with_structural_metadata(bib_id, params[:legacy_prefix])
-      create_record(bib_id, structural_record.to_xml, 'structural')
-      redirect "/records/#{bib_id}/show?format=structural"
-    end
-
-    blob = ''
-    path = "#{bibs_url}/?mms_id=#{bib_id}&expand=p_avail&apikey=#{alma_key}"
-
-    begin
-      open(path) { |io| blob = io.read }
-    rescue => exception
-      return "#{exception.message} returned by source for #{bib_id}"
-    end
-
-    return "No record data available at source for #{bib_id}" if blob.empty?
-
-    reader = Nokogiri::XML(blob)
-    record = reader.xpath('//bibs/bib/record')
-
-    holdings = {}
-
-    for i in 0..(record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children.length-1)
-      holdings[i] = { :holding_id => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children[i].text,
-                      :call_number => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="d"]').children[i].text,
-                      :library => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="b"]').children[i].text,
-                      :location => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="j"]').children[i].text
-      }
-    end
-
-    Nokogiri::XML::Builder.with(reader.at('record')) do |xml|
-      xml.holdings {
-        holdings.each do |holding_key, holding|
-          xml.holding {
-            xml.holding_id holding[:holding_id]
-            xml.call_number holding[:call_number]
-            xml.library holding[:library]
-            xml.location holding[:location]
+        for i in 0..(record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children.length-1)
+          holdings[i] = { :holding_id => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="8"]').children[i].text,
+                          :call_number => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="d"]').children[i].text,
+                          :library => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="b"]').children[i].text,
+                          :location => record.xpath('//record/datafield[@tag="AVA"]/subfield[@code="j"]').children[i].text
           }
         end
-      }
-    end
 
-    record.search('//record/datafield[@tag="INT"]').remove
-    record.search('//record/datafield[@tag="INST"]').remove
-    record.search('//record/datafield[@tag="AVA"]').remove
-
-    record = reader.xpath('//bibs/bib/record')
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim', 'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
-        xml << record.to_xml
-      }
-    end
-
-    create_record(bib_id, builder.to_xml, 'marc21')
-
-    redirect "/records/#{bib_id}/show?format=marc21"
-
-    Nokogiri::XML::Builder.with(reader.at('record')) do |xml|
-      xml.holdings {
-        holdings.each do |holding|
-          xml.holding {
-            xml.holding_id holding[:holding_id]
-            xml.call_number holding[:call_number]
-            xml.library holding[:library]
-            xml.location holding[:location]
+        Nokogiri::XML::Builder.with(reader.at('record')) do |xml|
+          xml.holdings {
+            holdings.each do |holding_key, holding|
+              xml.holding {
+                xml.holding_id holding[:holding_id]
+                xml.call_number holding[:call_number]
+                xml.library holding[:library]
+                xml.location holding[:location]
+              }
+            end
           }
         end
-      }
+
+        record.search('//record/datafield[@tag="INT"]').remove
+        record.search('//record/datafield[@tag="INST"]').remove
+        record.search('//record/datafield[@tag="AVA"]').remove
+
+        record = reader.xpath('//bibs/bib/record')
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim', 'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
+            xml << record.to_xml
+          }
+        end
+
+        create_record(bib_id, builder.to_xml, 'marc21')
+      when 'dla'
+        binding.pry
+      else
+        return "Invalid specified format #{format}"
     end
 
-    record.search('//record/datafield[@tag="INT"]').remove
-    record.search('//record/datafield[@tag="INST"]').remove
-    record.search('//record/datafield[@tag="AVA"]').remove
-
-    record = reader.xpath('//bibs/bib/record')
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim', 'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance', 'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
-        xml << record.to_xml
-      }
-    end
-
-    create_record(bib_id, builder.to_xml, 'marc21')
-
-    redirect "/records/#{bib_id}/show?format=marc21"
+    redirect "/records/#{bib_id}/show?format=#{format}"
   end
 
   get '/records/:bib_id/show/?' do |bib_id|
@@ -232,6 +177,7 @@ class Application < Sinatra::Base
     get path do
       @marc21_records = Record.where(:format => 'marc21')
       @structural_records = Record.where(:format => 'structural')
+      @dla_records = Record.where(:format => 'dla')
       erb :index
     end
   end
