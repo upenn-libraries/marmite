d49c3eae94bcca4231a51568e6b1a2dba6a0bc64
Mike Kanning <mkanning@upenn.edu>
2020-11-18 13:47:36 -0500

    add specs

--- spec/model/record_spec.rb
+++ spec/model/record_spec.rb
@@ -7,4 +7,34 @@ RSpec.describe Record, type: :model do
     expect(record).to respond_to(
       :bib_id, :format, :blob, :created_at, :updated_at)
   end
+  context 'freshness' do
+    it 'returns false if not an always recreated type' do
+      record = Record.new(
+        format: 'marc21',
+        updated_at: Time.now - 1.hour
+      )
+      expect(record.fresh?).to eq false
+    end
+    it 'returns true if an always recreated type' do
+      record = Record.new(
+        format: 'structural_ark',
+        updated_at: Time.now - 1.hour
+      )
+      expect(record.fresh?).to eq true
+    end
+    it 'returns false if more than one day old' do
+      record = Record.new(
+        format: 'marc21',
+        updated_at: Time.now - 2.days
+      )
+      expect(record.fresh?).to eq false
+    end
+    it 'returns true if less than one day old' do
+      record = Record.new(
+        format: 'marc21',
+        updated_at: Time.now - 1.hour
+      )
+      expect(record.fresh?).to eq false
+    end
+  end
 end
