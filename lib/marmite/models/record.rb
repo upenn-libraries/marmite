a959801526056b836e5471d1381071b866b6dfcd
Mike Kanning <mkanning@upenn.edu>
2020-11-18 12:34:21 -0500

    add fresh? instance method to Record class

--- lib/marmite/models/record.rb
+++ lib/marmite/models/record.rb
@@ -2,6 +2,8 @@ require 'sinatra/activerecord'
 
 class Record < ActiveRecord::Base
 
+  FORMATS_TO_ALWAYS_RECREATE = %w[structural_ark combined_ark]
+
   @error_message = ''
 
   def self.error_message
@@ -10,5 +12,20 @@ class Record < ActiveRecord::Base
 
   def self.error_message=(value)
     @error_message = value
+  end
+
+  # A record is fresh if it is:
+  # - considered one of the ALWAYS_FRESH_TYPES
+  # OR
+  # - less than 1 day old
+  # @return [TrueClass, FalseClass]
+  def fresh?
+    return true if format.in? FORMATS_TO_ALWAYS_RECREATE
+
+    return false unless updated_at
+
+    current_time = Time.now.gmtime
+    yesterday = current_time - 1.day
+    (yesterday..current_time).cover?(Time.at(updated_at).gmtime)
   end
 end
