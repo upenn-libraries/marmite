0083928673bb82e66e30c3816b3e5d394c17c4b4
Carla Galarza <cgalarza@upenn.edu>
2021-01-27 15:29:45 -0500

    Adding test for create_record. Still in progress

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,5 +1,28 @@
 RSpec.describe 'Application' do
   context 'create_record' do
+    context 'marc21' do
+      let(:bib_id) { '9951865503503681' }
+      let(:record) { Record.new(bib_id: bib_id, format: 'marc21') }
+      let(:alma_marc_xml) { File.read(File.join('spec', 'fixtures', 'marc', "#{bib_id}.xml")) }
+      let(:expected_xml) { '' } # fixture
 
+      before do
+        # mock api response
+        api_key = 'not_great'
+        ENV['ALMA_KEY'] = 'not_great'
+
+        stub_request(
+          :get,
+          "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/?mms_id=#{bib_id}&expand=p_avail&apikey=#{api_key}"
+        ).to_return(body: alma_marc_xml, headers: { 'Content-Type' => 'application/xml;charset=UTF-8' })
+
+        create_record(record)
+      end
+
+
+      it "add expected blob xml" do
+        expect(BlobHandler.uncompress(record.blob)).to be eql expected_xml
+      end
+    end
   end
 end
