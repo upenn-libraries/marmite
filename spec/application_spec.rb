d0f5617e9632b715f73f48c8b086f12a65e6eccc
lilblizzard <lilblizzard97@gmail.com>
2021-02-08 15:07:20 -0500

    test removal of INT, INST, AVA fields

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe 'Application' do
 
         # Mock the Alma API response
         stub_request(
-          :get,
-          "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/?mms_id=#{bib_id}&expand=p_avail&apikey=#{api_key}"
-        ).to_return(body: alma_marc_xml, headers: { 'Content-Type' => 'application/xml;charset=UTF-8' })
+            :get,
+            "https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/?mms_id=#{bib_id}&expand=p_avail&apikey=#{api_key}"
+        ).to_return(body: alma_marc_xml, headers: {'Content-Type' => 'application/xml;charset=UTF-8'})
 
         create_record(record) # calls the method to add blob to Record object
       end
@@ -27,20 +27,20 @@ RSpec.describe 'Application' do
       context 'when marc has 650 field, subfield a that begins with PRO' do
         let(:alma_marc_xml) do
           <<~MARC
-          <bibs total_record_count="1">
-            <bib>
-              <record>
-                <datafield ind1=" " ind2="0" tag="650">
-                  <subfield code="a">PRO Illumination of books and manuscripts, Carolingian</subfield>
-                  <subfield code="v">Specimens.</subfield>
-                </datafield>
-                <datafield ind1=" " ind2="0" tag="650">
-                  <subfield code="a">PRO Logic</subfield>
-                  <subfield code="v">Early works to 1800.</subfield>
-                </datafield>
-              </record>
-            </bib>
-          </bibs>
+            <bibs total_record_count="1">
+              <bib>
+                <record>
+                  <datafield ind1=" " ind2="0" tag="650">
+                    <subfield code="a">PRO Illumination of books and manuscripts, Carolingian</subfield>
+                    <subfield code="v">Specimens.</subfield>
+                  </datafield>
+                  <datafield ind1=" " ind2="0" tag="650">
+                    <subfield code="a">PRO Logic</subfield>
+                    <subfield code="v">Early works to 1800.</subfield>
+                  </datafield>
+                </record>
+              </bib>
+            </bibs>
           MARC
         end
 
@@ -79,47 +79,47 @@ RSpec.describe 'Application' do
       context 'when marc has 650 fields, subfield a that begins with CHR' do
         let(:alma_marc_xml) do
           <<~MARC
-          <bibs total_record_count="1">
-            <bib>
-              <record>
-                <datafield ind1=" " ind2="0" tag="650">
-                  <subfield code="a">CHR Illumination of books and manuscripts, Carolingian</subfield>
-                  <subfield code="v">Specimens.</subfield>
-                </datafield>
-                <datafield ind1=" " ind2="0" tag="650">
-                  <subfield code="a">CHR Logic</subfield>
-                  <subfield code="v">Early works to 1800.</subfield>
-                </datafield>
-              </record>
-            </bib>
-          </bibs>
+            <bibs total_record_count="1">
+              <bib>
+                <record>
+                  <datafield ind1=" " ind2="0" tag="650">
+                    <subfield code="a">CHR Illumination of books and manuscripts, Carolingian</subfield>
+                    <subfield code="v">Specimens.</subfield>
+                  </datafield>
+                  <datafield ind1=" " ind2="0" tag="650">
+                    <subfield code="a">CHR Logic</subfield>
+                    <subfield code="v">Early works to 1800.</subfield>
+                  </datafield>
+                </record>
+              </bib>
+            </bibs>
           MARC
         end
 
         let(:expected_xml) do
           <<~OUT
-          <?xml version="1.0"?>
-          <marc:records xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
-            <marc:record>
-              <marc:datafield ind1=" " ind2="0" tag="650">
-            <marc:subfield code="v">Specimens.</marc:subfield>
-          </marc:datafield>
-              <marc:datafield ind1=" " ind2="0" tag="650">
-            <marc:subfield code="v">Early works to 1800.</marc:subfield>
-          </marc:datafield>
-              <marc:datafield ind1=" " ind2=" " tag="651">
-            <marc:subfield code="y">Illumination of books and manuscripts, Carolingian</marc:subfield>
-          </marc:datafield>
-              <marc:datafield ind1=" " ind2=" " tag="651">
-            <marc:subfield code="y">Logic</marc:subfield>
-          </marc:datafield>
-              <marc:datafield ind1=" " ind2=" " tag="999">
-            <marc:subfield code="z">CHR Illumination of books and manuscripts, Carolingian</marc:subfield>
-            <marc:subfield code="z">CHR Logic</marc:subfield>
-          </marc:datafield>
-              <marc:holdings/>
-            </marc:record>
-          </marc:records>
+            <?xml version="1.0"?>
+            <marc:records xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
+              <marc:record>
+                <marc:datafield ind1=" " ind2="0" tag="650">
+              <marc:subfield code="v">Specimens.</marc:subfield>
+            </marc:datafield>
+                <marc:datafield ind1=" " ind2="0" tag="650">
+              <marc:subfield code="v">Early works to 1800.</marc:subfield>
+            </marc:datafield>
+                <marc:datafield ind1=" " ind2=" " tag="651">
+              <marc:subfield code="y">Illumination of books and manuscripts, Carolingian</marc:subfield>
+            </marc:datafield>
+                <marc:datafield ind1=" " ind2=" " tag="651">
+              <marc:subfield code="y">Logic</marc:subfield>
+            </marc:datafield>
+                <marc:datafield ind1=" " ind2=" " tag="999">
+              <marc:subfield code="z">CHR Illumination of books and manuscripts, Carolingian</marc:subfield>
+              <marc:subfield code="z">CHR Logic</marc:subfield>
+            </marc:datafield>
+                <marc:holdings/>
+              </marc:record>
+            </marc:records>
           OUT
         end
 
@@ -129,7 +129,35 @@ RSpec.describe 'Application' do
       end
 
       context 'when marc has INT, INST and AVA fields' do
-        it 'they are removed'
+        let(:alma_marc_xml) do
+          <<~MARC
+            <bibs total_record_count="1">
+              <bib>
+                <record>
+                  <datafield ind1=" " ind2="0" tag="INT">
+                  </datafield>
+                  <datafield ind1=" " ind2="0" tag="INST">
+                  </datafield>
+                  <datafield ind1=" " ind2="0" tag="AVA">
+                  </datafield>
+                </record>
+              </bib>
+            </bibs>
+          MARC
+        end
+        let(:expected_xml) do
+          <<~OUT
+            <?xml version="1.0"?>
+            <marc:records xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
+              <marc:record>
+                <marc:holdings/>
+              </marc:record>
+            </marc:records>
+          OUT
+        end
+        it 'they are removed' do
+          expect(BlobHandler.uncompress(record.blob)).to eq expected_xml
+        end
       end
 
       context 'when marc has more than one holdings information' do
