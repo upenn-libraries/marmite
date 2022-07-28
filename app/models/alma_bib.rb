b2984a149d65100586358ed3644196d95e10c0d2
Mike Kanning <mkanning@upenn.edu>
2022-07-28 10:48:14 -0400

    address Rubocop complaints from latest additions

--- app/models/alma_bib.rb
+++ app/models/alma_bib.rb
@@ -75,7 +75,7 @@ class AlmaBib
   def build_new_marcxml(leader, control, holdings, all_datafields)
     builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
       xml['marc'].records('xmlns:marc' => 'http://www.loc.gov/MARC21/slim',
-                          'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance',
+                          'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
                           'xsi:schemaLocation' => 'http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd') {
         xml.record {
           xml << leader.to_xml
--- spec/models/alma_bib_spec.rb
+++ spec/models/alma_bib_spec.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 RSpec.describe AlmaBib, type: :model do
-  let(:alma_bib) { AlmaBib.new xml }
-  context 'for XML with Arabic script' do
+  let(:alma_bib) { described_class.new xml }
+
+  context 'with XML including Arabic script' do
     let(:xml) { marc21_pre_transform 'record_with_arabic' }
 
     it 'is transformed and retains properly encoded Arabic script' do
