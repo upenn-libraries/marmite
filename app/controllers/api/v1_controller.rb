c4adb33a1536507e0fbaa03fdc363c26c8993e1d
Mike Kanning <mkanning@upenn.edu>
2022-09-07 13:16:03 -0400

    link to app homepage rather than GitLab page in response to v1 API requests.

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -2,14 +2,10 @@ require_relative '../base_controller'
 
 class Api
   class V1Controller < BaseController
-    # return message to any continued V1 API users, and log to Honybadger
+    # return message to any continued V1 API users, and log to Honeybadger
     get '/records*' do
       Honeybadger.notify('Legacy V1 API request')
-      [
-        303, # see other
-        ['Please use Marmite V2 API. ',
-         'See <a href="https://gitlab.library.upenn.edu/digital-repository/marmite">the documentation</a>.'].join
-      ]
+      [303, 'Please use Marmite V2 API. See <a href="/">the documentation</a>.']
     end
   end
 end
